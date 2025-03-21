dfcc054dadb60668ba245b72f3a9af5b8a406933
palec87 <david@stanka.de>
2025-03-21 13:46:40 +0000

    try 1

--- python_src/prepare_data.py
+++ python_src/prepare_data.py
@@ -83,8 +83,11 @@ def main(
         Path(outpath).mkdir()
 
     bzip2_program = find_bzip2()
-
-    run_id = Path(str(archive_name).rsplit(".", 2)[0])
+    if hcmr:
+        log.debug("HCMR data")
+        run_id = Path(str("DBB_AACDOSDA_4_HMGW5DSX3.UDI240.zip").rsplit(".", 1)[0].split("_")[-1])
+    else:
+        run_id = Path(str(archive_name).rsplit(".", 2)[0])
 
     oca_dir = Path(outpath, f"{run_id}")
     if oca_dir.exists():
