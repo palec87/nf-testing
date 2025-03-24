37acb51652f94250d82fedd97e8a913cddcce90d
palec87 <david@stanka.de>
2025-03-24 11:24:37 +0000

    better

--- python_src/extract_hcmr_data.py
+++ python_src/extract_hcmr_data.py
@@ -13,30 +13,32 @@ for subfolder in SUBFOLDERS:
     # list folders in subfolder
     folders = os.listdir(subfolder_path)
     print("####### 1. layer folders ######")
-    print(folders)
+    print('subfolders', folders)
 
     folders_to_keep = []
-    for folder in os.path.abspath(folders):
-        if "DBH" or "DBB" in folder:
+    for folder in folders:
+        if "DBH" in str(folder) or "DBB" in str(folder):
             folders_to_keep.append(folder)
             continue
-        if "results" not in folder:
+        if "results" not in (folder):
             continue
 
         # this is already the archive
-        parent_folder = os.path.join(subfolder_path, folder).split("/")[-1].split("_")[-1] 
-        print(parent_folder)
+
+        parent_folder = subfolder_path.split("/")[-1].split("_")[-1] 
+        print('parent folder', parent_folder, 'of', folder)
 
     #next layer
     print("####### 2. layer folders ######")
+    folders_to_keep2 = []
     print(folders_to_keep)
-    for folder in os.path.abspath(folders_to_keep):
-        if "DBH" or "DBB" in folder:
-            folders_to_keep.append(folder)
+    for folder in folders_to_keep:
+        if "DBH" in str(folder) or "DBB" in str(folder):
+            folders_to_keep2.append(folder)
             continue
-        if "results" not in folder:
+        if "results" not in str(folder):
             continue
 
         # this is already the archive
         parent_folder = os.path.join(subfolder_path, folder).split("/")[-1].split("_")[-1] 
-        print(parent_folder)
+        print('parent folder', parent_folder, 'of', folder)
