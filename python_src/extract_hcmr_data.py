26072c6bed91ff1c82a51dee46613a569cd6cb80
palec87 <david@stanka.de>
2025-03-24 10:40:58 +0000

    more listing

--- python_src/extract_hcmr_data.py
+++ python_src/extract_hcmr_data.py
@@ -9,13 +9,34 @@ SUBFOLDERS = ["220223_JARVIS_HWLTKDRXY",
 
 for subfolder in SUBFOLDERS:
     subfolder_path = os.path.join(ROOT_FOLDER, subfolder)
-    print(subfolder_path)
+    # print(subfolder_path)
     # list folders in subfolder
     folders = os.listdir(subfolder_path)
+    print("####### 1. layer folders ######")
+    print(folders)
+
+    folders_to_keep = []
     for folder in folders:
+        if "DBH" or "DBB" in folder:
+            folders_to_keep.append(folder)
+            continue
         if "results" not in folder:
             continue
 
+        # this is already the archive
+        parent_folder = os.path.join(subfolder_path, folder).split("/")[-1].split("_")[-1] 
+        print(parent_folder)
 
-        folder_path = os.path.join(subfolder_path, folder)
-        print(folder_path)
+    #next layer
+    print("####### 2. layer folders ######")
+    print(folders_to_keep)
+    for folder in folders_to_keep:
+        if "DBH" or "DBB" in folder:
+            folders_to_keep.append(folder)
+            continue
+        if "results" not in folder:
+            continue
+
+        # this is already the archive
+        parent_folder = os.path.join(subfolder_path, folder).split("/")[-1].split("_")[-1] 
+        print(parent_folder)
